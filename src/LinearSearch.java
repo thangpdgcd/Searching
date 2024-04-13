2932ebcce3007a12bd4d0d65849d888409edd235
thangpdgcd <thangpdgcd201784@fpt.edu.vn>
2024-04-13 10:18:09 +0700

    new searching

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class LinearSearch {
+public class LinearSearch {
+    public static int search(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+class runnercode {
+    public static void main(String[] args) {
+        int[] numbers = {7, 1, 3, 6, 5};
+        LinearSearch searcher = new LinearSearch();
+        int target = 0;
+        int index = LinearSearch.search(numbers, target);
+//        int index = LinearleSearch.search(numbers);
+        System.out.println(" this is a number searching " + index);
+
+    }
 }
